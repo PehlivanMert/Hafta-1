8430875ceb6ae9e90d9ee6917cc727c1b0d0b330
PehlivanMert <pehlivannmert@gmail.com>
2023-12-12 15:12:36 +0300

    Vki Hesaplama

--- VucutKitleIndeksi/src/Main.java
+++ VucutKitleIndeksi/src/Main.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //Değişkenleri tanımlıyoruz
         int kilo;
         double boy,vki;
+
+        //Scanner sınıfımızı oluşturuyoruz
         Scanner girdi = new Scanner(System.in);
 
-        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        //Kullanıcıdan boy ve kilo bilgisini alıyoruz
+        System.out.print("Lütfen boyunuzu (metre cinsinden virgül ile) giriniz : ");
         boy = girdi.nextDouble();
+
         System.out.print("Lütfen kilonuzu giriniz : ");
         kilo = girdi.nextInt();
 
+        //Vücut Kitle İndeksini hesaplıyoruz
         vki =  kilo / (boy * boy);
 
+        //Sonucu ekrana yazdırıyoruz.
         System.out.println("Vücut Kitle İndeksiniz: " + vki);
 
     }
