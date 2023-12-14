ae47a1726ace624b8a3bad7bad9f7712af59e04b
PehlivanMert <pehlivannmert@gmail.com>
2023-12-15 01:45:57 +0300

    .

--- NewFeedback/src/Main.java
+++ NewFeedback/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -42,20 +42,3 @@ public class Main {
         }
 
 */
-        //Degisken tanimlama
-        int year;
-
-        Scanner inp = new Scanner(System.in);
-
-        System.out.println("Yıl Giriniz:");
-        year = inp.nextInt();
-
-        // 4 rakamının katı ve 100'ün katı olmayanlar veya 400'e kalansız olarak bölünebilenler
-        if ((year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0){
-            System.out.println(year+" bir artık yıldır");
-        } else {
-            System.out.println(year+" bir artık yıl değildir !");
-        }
-
-    }
-}
