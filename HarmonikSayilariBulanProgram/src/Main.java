1e5bf81cfc241d228336206a27200e8ecc86c8dd
PehlivanMert <pehlivannmert@gmail.com>
2023-12-14 17:53:30 +0300

    Harmonik Seri

---
+++ HarmonikSayilariBulanProgram/src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz : ");
+        int sayi = input.nextInt();
+        double result = 0.0;
+        for (int i = 1 ; i <= sayi; i++) {
+            result += (1.0/i);
+        }
+        System.out.println(result);
+    }
+}
