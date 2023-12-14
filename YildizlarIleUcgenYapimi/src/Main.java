6e4fa12c2b578c8dcd0ad9d5ada329730de3bd05
PehlivanMert <pehlivannmert@gmail.com>
2023-12-14 18:09:35 +0300

    Yilızlarla üçgen yapımı

---
+++ YildizlarIleUcgenYapimi/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int n,yildiz,bosluk;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak sayısını giriniz : ");
+        n = input.nextInt();
+
+        for (int i = 1 ; i <= n ; i++ ){
+
+            for(bosluk = 1 ; bosluk <= (n - i) ; bosluk++){
+                System.out.print(" ");
+            }
+            for(yildiz = 1 ; yildiz <= ((2 * i)-1); yildiz++) {
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
