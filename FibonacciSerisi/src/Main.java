7d07a54367f23c7fbed729740b2e2f4e281c2a55
PehlivanMert <pehlivannmert@gmail.com>
2023-12-16 22:50:40 +0300

    Version 2

--- FibonacciSerisi/src/Main.java
+++ FibonacciSerisi/src/Main.java
@@ -5,16 +5,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Fibonacci eleman sayısını giriniz: ");
         int n = input.nextInt();
-        int a = 0, b = 1, c = 0;                                                //İlk iki elemanı tanımlıyoruz ve yazdırıyoruz
+        int a = 0, b = 1, c = 0;                                                //İlk iki değişkeni tanımlıyoruz ve yazdırıyoruz
 
-        System.out.print(n + " Elemanlı Fibonacci Serisi : " + a + " " + b);    //İlk iki elemanı yazdırıyoruz
+        System.out.print(n + " Elemanlı Fibonacci Serisi : " + a + " " + b);
 
         for (int i = 2; i < n; i++) {
-            c = a + b;                                                          //İlk iki elemanı toplayıp üçüncü elemanı buluyoruz
+            c = a + b;                                                          //İlk iki elemanı yani a+ b toplayıp c'ye atıyoruz
             System.out.print(" " + c);
-            a = b;                                                               //İlk elemanı ikinci elemana eşitliyoruz
-            b = c;                                                               //İkinci elemanı üçüncü elemana eşitliyoruz
-        }                                                                        //Bu işlemi n'e kadar tekrarlıyoruz
+            a = b;                                                               //a'yı b'ye  atıyoruz
+            b = c;                                                               //b'ye c'yi atıyoruz
+        }                                                                        //Bu işlemi n kadar tekrar ediyoruz.
 
     }
 }
