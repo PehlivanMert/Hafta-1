7da209572846be70aad0f649a1109b410e9c6f8a
PehlivanMert <pehlivannmert@gmail.com>
2023-12-16 22:39:16 +0300

    Version 2

--- AsalSayilar/src/Main.java
+++ AsalSayilar/src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int sayi;
-        for (int i = 2; i < 100; i++) {             // For döngüsü oluşturuyoruz. 2'den 100'e kadar sayıları kontrol edeceğiz.
-            sayi = 0;
 
+        for (int i = 2; i <= 100; i++) {
+
+            int counter = 0;                        // For döngüsü oluşturuyoruz. 2'den 100'e kadar sayıları kontrol edeceğiz.
             for (int j = 2; j < i; j++) {           //Sayıyı 2'den başlatıyoruz çünkü 1 asal sayı değildir.
                 if (i % j == 0) {                   //Sayıyı kendisine kadar bölen bir sayı var mı diye bakıyoruz.
-                    sayi++;                         //Eğer varsa sayı değişkenini arttırıyoruz
+                    counter++;                         //Eğer varsa counter değişkenini arttırıyoruz
                 }
-            }                                        //Eğer sayı değişkeni 0'dan farklıysa asal sayı değildir.
-            if (sayi == 0) {
+            }                                        //Eğer counter değişkeni 0'dan farklıysa asal sayı değildir.
+
+            if (counter == 0) {
                 System.out.print(i + " - ");
             }
         }
--- FibonacciSerisi/src/Main.java
+++ FibonacciSerisi/src/Main.java
@@ -5,16 +5,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.print("Fibonacci eleman sayısını giriniz: ");
         int n = input.nextInt();
-        int a = 0, b = 1, c = 0;                                //İlk iki elemanı tanımlıyoruz ve yazdırıyoruz
+        int a = 0, b = 1, c = 0;                                                //İlk iki elemanı tanımlıyoruz ve yazdırıyoruz
 
-        System.out.print(a + " " + b);                          //İlk iki elemanı yazdırıyoruz
+        System.out.print(n + " Elemanlı Fibonacci Serisi : " + a + " " + b);    //İlk iki elemanı yazdırıyoruz
 
         for (int i = 2; i < n; i++) {
-            c = a + b;                                          //İlk iki elemanı toplayıp üçüncü elemanı buluyoruz
+            c = a + b;                                                          //İlk iki elemanı toplayıp üçüncü elemanı buluyoruz
             System.out.print(" " + c);
-            a = b;                                              //İlk elemanı ikinci elemana eşitliyoruz
-            b = c;                                              //İkinci elemanı üçüncü elemana eşitliyoruz
-        }                                                       //Bu işlemi n'e kadar tekrarlıyoruz
+            a = b;                                                               //İlk elemanı ikinci elemana eşitliyoruz
+            b = c;                                                               //İkinci elemanı üçüncü elemana eşitliyoruz
+        }                                                                        //Bu işlemi n'e kadar tekrarlıyoruz
 
     }
 }
